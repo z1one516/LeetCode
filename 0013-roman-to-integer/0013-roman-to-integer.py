9c8eed1e2125a422baa7c15319abb8bb8f10137a
Jiwon <126679650+z1one516@users.noreply.github.com>
2023-07-14 14:40:26 +0900

    Time: 59 ms (80.42%), Space: 16.4 MB (50.32%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,13 +1,24 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        str_list = list(s)
+      #   str_list = list(s)
         roman = {'I': 1, 'V': 5, 'X':10, 'L':50, 'C':100,'D':500,'M':1000}
-        answer = roman[str_list[-1]]
-      # 뒤에 나오는 로만 숫자가 더 클 때 str_list[idx+1] - str_list[idx] 
-      # 원래는 str_list[idx] + str_list[idx+1]
-        for idx,k in enumerate(str_list[:-1]):
-            if roman[k] < roman[str_list[idx+1]]:
-                answer -= roman[k]
+      #   answer = roman[str_list[-1]]
+      # # 뒤에 나오는 로만 숫자가 더 클 때 str_list[idx+1] - str_list[idx] 
+      # # 원래는 str_list[idx] + str_list[idx+1]
+      #   for idx,k in enumerate(str_list[:-1]):
+      #       if roman[k] < roman[str_list[idx+1]]:
+      #           answer -= roman[k]
+      #       else:
+      #           answer += roman[k]   
+      #   return answer
+    
+    
+      # 1. string 도 index로 접근 가능
+      # 2. 마지막 문자는 무조건 더하기  
+        tot = roman[s[-1]]
+        for i in range(len(s)-1):
+            if roman[s[i]] < roman[s[i+1]]:
+                tot -= roman[s[i]]
             else:
-                answer += roman[k]   
-        return answer
+                tot += roman[s[i]]
+        return tot
