54263de333d7e5c1b8fb67d945c06d25197bb96b
Jiwon <126679650+z1one516@users.noreply.github.com>
2023-07-25 22:11:49 +0900

    Time: 55 ms (17.28%), Space: 16.3 MB (69.78%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,7 +1,18 @@
+# class Solution:       
+#     def plusOne(self, digits) :
+#       num = "" 
+#       for i in digits:
+#         num += str(i)
+#       answer = [int(i) for i in str(int(num)+1)]
+#       return answer
 class Solution:       
     def plusOne(self, digits) :
-      num = "" 
-      for i in digits:
-        num += str(i)
-      answer = [int(i) for i in str(int(num)+1)]
-      return answer
+      if len(digits) == 1 and digits[0] == 9 :
+        return [1,0]
+      elif digits[-1] < 9:
+        digits[-1] +=1
+        return digits
+      else: # digits[-1] ==9, len(digits) > 1
+        digits[-1] = 0
+        digits[0:-1] =self.plusOne(digits[0:-1])
+        return digits
