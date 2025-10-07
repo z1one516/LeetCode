921402ac47ef61c480d1380e99b8a8db84fcfee7
Jiwon <126679650+z1one516@users.noreply.github.com>
2025-10-07 15:02:07 -0400

    Time: 474 ms (27.09%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN 
+    DECLARE result INT;
+    SET N = N - 1; 
+
+    SELECT DISTINCT salary
+    INTO result
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET N;
+
+    RETURN result;
+
+END
